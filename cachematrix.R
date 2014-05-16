3725653a16d70d5d64620e2594cb78ff82cb05bc
yeckley <joe.yeckley@gmail.com>
2014-05-15 22:27:01 -0400

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2 for JHU Intro to R cours via Coursera. This 
+## assignmnet demonstrates the use of lexical scoping to implement a caching
+## scheme to improve efficiency of R programs that may need to repeat
+## computationally expensive tasks.
 
-## Write a short comment describing this function
+## It assumes the some environment will call makeCacheMatrix() from an
+## environment (e.g. the global environment) assign the returned list to some
+## object, which is then passed to cacheSolve().
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  ## when setting we no longer have a cached inverse
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(x) {
+    message("That's new. I'll save that.",appendLF=TRUE)
+    inv <<- x
+  }
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
+## Get the inverse of a matrix. If the matrix is already cached
+## use that to save some cycles. Otherwise, calculate it and 
+## cache it.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x) {
+  ## Check whether the cached inverse is non-null and, if so, return it.
+  if (!is.null(x$getinv())) {
+      message('Returning cached matrix.', appendLF=TRUE)
+      return(x$getinv())
+    }
+  ## If the inverse matrix cache was empty, set it now and then return it.
+  x$setinv(solve(x$get()))
+  x$getinv()
+}        
